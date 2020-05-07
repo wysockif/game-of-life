5032eaac740312ec92b0650476c68e3dc1260b24
Franek <wysocki.f.00@gmail.com>
2020-05-07 15:55:36 +0200

    Turning down the sounds

--- BattleZone/src/main/java/Sounds.java
+++ BattleZone/src/main/java/Sounds.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.swing.JOptionPane;
 
@@ -15,6 +16,7 @@ public class Sounds {
                     AudioInputStream inputStream = AudioSystem.getAudioInputStream(
                             Sounds.class.getResource("sounds/TankFiring.wav"));
                     clip.open(inputStream);
+                    turnDown(clip);
                     clip.start();
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null,
@@ -32,10 +34,11 @@ public class Sounds {
                     AudioInputStream inputStream = AudioSystem.getAudioInputStream(
                             Sounds.class.getResource("sounds/GamingPoint.wav"));
                     clip.open(inputStream);
+                    turnDown(clip);
                     clip.start();
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null,
-                            "Błąd z dźwiękiem wystrzału!", "Błąd!", JOptionPane.ERROR_MESSAGE);
+                            "Błąd z dźwiękiem zestrzelenia komórki!", "Błąd!", JOptionPane.ERROR_MESSAGE);
                 }
             }
         }).start();
@@ -50,13 +53,19 @@ public class Sounds {
                     AudioInputStream inputStream = AudioSystem.getAudioInputStream(
                             Sounds.class.getResource("sounds/GameOver.wav"));
                     clip.open(inputStream);
+                    turnDown(clip);
                     clip.start();
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null,
-                            "Błąd z dźwiękiem wystrzału!", "Błąd!", JOptionPane.ERROR_MESSAGE);
+                            "Błąd z dźwiękiem skończonej gry!", "Błąd!", JOptionPane.ERROR_MESSAGE);
                 }
             }
         }).start();
+    }
+
+    private static void turnDown(Clip clip) {
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(0.05f));
     }
 }
 
